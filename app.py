1b2cd38198150e80586ad42a978af8a0ab1f395f
alcaloide2000 <126974111+alcaloide2000@users.noreply.github.com>
2024-02-05 17:13:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -80,6 +80,131 @@ card2 = dmc.Card(
     style={"width": 350},
 )
 
+card3 = dmc.Card(
+    children=[
+        dmc.CardSection(
+            dmc.Anchor(
+                dmc.Image(
+                    src="https://i.im.ge/2024/01/23/YVJpED.thomas.png",
+                    alt="dash-app",
+                ),
+                href="https://theec5.onrender.com/",
+                target="_blank"
+            ),
+        ),
+        dmc.Group(
+            [
+                dmc.Text("TRANSLATION ESP/ENG GAME", weight=500, size='xl'),
+                html.A(
+                    DashIconify(icon="ion:logo-github", width=30),
+                    href='https://github.com/alcaloide2000/theec5',
+                    target="_blank"
+                )
+            ],
+            position="apart",
+            mt="md",
+            mb="xs",
+        ),
+        dmc.Text(
+            "basic translation game from Spanish to English sentences",
+            size="sm",
+            color="dimmed",
+        ),
+    ],
+    withBorder=True,
+    shadow="sm",
+    radius="md",
+    style={"width": 350},
+)
+
+all_cards = [
+    dmc.Header(
+        height=80,
+        children=[dmc.Text("Data Analysis and AI Projects",
+                           style={"fontSize": 40})],
+    ),
+    dmc.SimpleGrid(
+        cols=3,
+        spacing="lg",
+        breakpoints=[
+            {"maxWidth": 1240, "cols": 2, "spacing": "md"},
+            {"maxWidth": 950, "cols": 1, "spacing": "sm"},
+        ],
+        children=[
+            html.Div(card1),
+            html.Div(card2),
+            html.Div(card3),
+        ],
+    )
+]
+
+reference_card = html.Div([
+    dmc.Card(
+        children=[
+            dmc.Text("ALCALOIDE", weight=500, size='xl'),
+            dmc.Text(
+                "Basic apps in Dash/Python",
+                size="md",
+                mb="xs",
+            ),
+            html.A(
+                DashIconify(icon="bi:twitter", width=30),
+                href='https://twitter.com/alcaloide',
+                target="_blank"
+            ),
+            html.A(
+                DashIconify(icon="ion:logo-github", width=30),
+                href='https://github.com/alcaloide2000',
+                target="_blank"
+            ),
+            html.A(
+                    DashIconify(icon="bi:discord", width=30),
+                    href='https://discord.com/',
+                    target="_blank"
+                )
+
+        ],
+        withBorder=True,
+        shadow="sm",
+        radius="md",
+        style={"width": 350})
+    ],
+    style={"paddingTop": 40}
+)
+
+resume_div = html.Div([
+    html.Iframe(src="https://www.dropbox.com/scl/fi/199571mr76sse93cwr8q5/dashmultipagecheatsheet.pdf?rlkey=ekhlp6c2h82ewy5og41k4v657&dl=0",
+                width="800", height="480")
+    ],
+    style={"paddingTop": 40}
+)
+
+
+app = Dash()
+server = app.server
+app.layout = dmc.MantineProvider(
+    theme={"colorScheme": "dark"},
+    withGlobalStyles=True,
+    children=[
+            dmc.Tabs(
+        [
+            dmc.TabsList(
+                [
+                    dmc.Tab("Projects", value="projects"),
+                    dmc.Tab("Cheatsheets", value="resume"),
+                    dmc.Tab("Links", value="references"),
+                ], style={"paddingRight": 50, "paddingTop": 15}
+            ),
+            dmc.TabsPanel(children=all_cards, value="projects", pb="xs"),
+            dmc.TabsPanel(resume_div, value="resume", pb="xs"),
+            dmc.TabsPanel(reference_card, value="references", pb="xs"),
+        ],
+        value="projects",
+        orientation='vertical',
+        variant='pills',
+    )
+])
+
 
 if __name__=='__main__':
     app.run(debug=False)
